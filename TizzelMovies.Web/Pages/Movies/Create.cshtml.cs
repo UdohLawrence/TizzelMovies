794ea34579080a08bac16a527c2f4ed6dc52a984
UdohLawrence <lawrenceaudoh@gmail.com>
2025-12-24 21:01:57 +0100

    Scaffold pages for the movies resource

---
+++ TizzelMovies.Web/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TizzelMovies.Web.Data;
+using TizzelMovies.Web.Models;
+
+namespace TizzelMovies.Web.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly TizzelMovies.Web.Data.MoviesContext _context;
+
+        public CreateModel(TizzelMovies.Web.Data.MoviesContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
