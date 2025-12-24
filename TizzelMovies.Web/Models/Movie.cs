6dd29a78ffcc274a6d945003e45dccf2a2a36afd
UdohLawrence <lawrenceaudoh@gmail.com>
2025-12-24 21:01:22 +0100

    Add a data model for the movies

---
+++ TizzelMovies.Web/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TizzelMovies.Web.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string? Title { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
+        public DateOnly ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
