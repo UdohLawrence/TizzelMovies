e8ce312f36488c2ae5db6d8930bf631f8891b0cf
UdohLawrence <lawrenceaudoh@gmail.com>
2025-12-24 21:04:14 +0100

    Update appsettings and Program.cs to takek care of data migrations

--- TizzelMovies.Web/Program.cs
+++ TizzelMovies.Web/Program.cs
@@ -1,7 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TizzelMovies.Web.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<MoviesContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("MoviesContext") ?? throw new InvalidOperationException("Connection string 'MoviesContext' not found.")));
 
 var app = builder.Build();
 
